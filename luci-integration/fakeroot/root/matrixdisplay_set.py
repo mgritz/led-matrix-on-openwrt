6590b9a250ce372fc46800573f9ed6d8002cbcc5
Martin Gritzan <martin.gritzan@gmail.com>
2022-11-01 14:56:44 +0100

    add python script for updating matrix

---
+++ luci-integration/fakeroot/root/matrixdisplay_set.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os
+import serial
+from time import sleep
+
+CFGFILE = '/etc/config/matrixdisplay'
+SERIALIFACE = '/dev/ttyS0'
+
+lastchange=0
+
+while True:
+    changed = os.path.getmtime(CFGFILE)
+    if changed != lastchange:
+        lastchange = changed
+        content = ""
+        with open(CFGFILE, 'r') as f:
+            content = f.read()
+
+        text = content.split("'")[3] # now this is a bit of a hack, of course, but well...
+
+        with serial.Serial(SERIALIFACE, 9600) as s:
+            s.write(text.encode('ascii') + b'\n')
+
+        print("text changed to \"{}\"".format(text))
+
+    sleep(10)
+        
+
+
